c02cab8c053ccb7853c1b13e745a5d4b2d18373e
Mohd. Saif Siddiqui <siddiqui16saif@gmail.com>
2025-07-24 17:13:11 +0530

    correct datauri

--- src/utils/dataUri.ts
+++ src/utils/dataUri.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 export const getBuffer = (file: any) => {
   const parser = new DataURIParser();
-  const ext = path.extname(file.oridinalname);
+  const ext = path.extname(file.oridinalname).toString();
   const dataUri = parser.format(ext, file.buffer);
   return dataUri;
 };
