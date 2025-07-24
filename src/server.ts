c7b743baad11779b38e40e9190d93f8371cfd1cf
Mohd. Saif Siddiqui <siddiqui16saif@gmail.com>
2025-07-24 16:58:13 +0530

    added dynamic frontend

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
   })
 );
 
